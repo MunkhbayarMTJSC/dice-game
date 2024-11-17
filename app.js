eef5699c35517f0fa16d044b9b9133e2b8b5401e
SiCA <munkhbayar.pch@gmail.com>
2024-11-17 14:03:45 +0800

    toglogchiin eeljiig solidog blov

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var activePlayer = 0;
 var scores= [0, 0];
 // Ээлжийн оноо
 var roundScore = 0;
-// Шоо хаях үед 1-6 хүртэлх санамсаргүйгээр хадгалах хувьсагч хэрэгтэй
-var diceNumber = Math.floor(Math.random() * 6) + 1;
 // Программ эхлэхэд бэлтгэх
 document.getElementById("score-0").textContent = 0;
 document.getElementById("score-1").textContent = 0;
@@ -20,6 +18,26 @@ diceDom.style.display = "none";
 document.querySelector(".btn-roll").addEventListener("click", function () {
     // Шоо хаях үед 1-6 хүртэлх санамсаргүйгээр хадгалах хувьсагч хэрэгтэй
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+    // Шооны зургийг вэб дээр гаргаж ирнэ
     diceDom.style.display = "block";
+    // Шоо хаяхад буусан зургийг харгалзуулж харуулна
     diceDom.src = "dice-" + diceNumber + ".png";
+
+    // Буусан тоо нь нэгээс ялгаатай бол идэвхтэй тоглогчийн тоог нэмэгдүүлнэ.
+    if (diceNumber !== 1) {
+        // 1 ээс ялгаатай буусан тул тоглогчийн ээлжийн оноог нэмнэ
+        roundScore += diceNumber;
+        document.getElementById("current-"+activePlayer).textContent = roundScore;
+    } else {
+        // 1 буусан тул тоглогчийн ээлжийг солино
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        document.querySelector(".player-"+ activePlayer + "-panel").classList.remove('active')
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        //Улаан цэгийг шилжүүлнэ
+        document.querySelector(".player-"+ activePlayer + "-panel").classList.add('active')
+        diceDom.style.display = "none"
+    }
+    ;
+    
 });
